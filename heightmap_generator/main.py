735dc2c4e85b094b59e097f269eaf933cd9e3e08
Arti Schmidt <artischmidt3@gmail.com>
2022-12-08 13:10:55 +0100

    Complete basic heightmap generator

--- heightmap_generator/main.py
+++ heightmap_generator/main.py
@@ -1,30 +1,81 @@
 import os
+import json
 import requests
+import numpy as np
 import geopy
 import geopy.distance
+import rasterio
+from rasterio.crs import CRS
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+from PIL import Image
 
-MAPS_API_KEY = os.environ['MAPS_API_KEY']
-OPEN_TOPOGRAPHY_API_KEY = os.environ['OPEN_TOPOGRAPHY_API_KEY']
 
-center = geopy.Point(latitude=45.278056, longitude=-111.450278)
-radius = geopy.distance.distance(meters=1000)
+def get_dem(dem_filename, center, radius):
+    distance = geopy.distance.distance(meters=radius)
+    response = requests.get(
+        'https://portal.opentopography.org/API/globaldem',
+        {
+            'API_Key': os.environ['OPEN_TOPOGRAPHY_API_KEY'],
+            'demtype': 'COP30',
+            'outputFormat': 'GTiff',
+            'north': distance.destination(center, bearing=0).latitude,
+            'south': distance.destination(center, bearing=180).latitude,
+            'east': distance.destination(center, bearing=90).longitude,
+            'west': distance.destination(center, bearing=270).longitude,
+        }
+    )
+    with open(dem_filename, 'wb') as file:
+        file.write(response.content)
 
-north_bound = radius.destination(center, bearing=0).latitude
-east_bound = radius.destination(center, bearing=90).longitude
-south_bound = radius.destination(center, bearing=180).latitude
-west_bound = radius.destination(center, bearing=270).longitude
 
-response = requests.get(
-    'https://portal.opentopography.org/API/globaldem',
-    {
-        'demtype': 'COP30',
-        'north': north_bound,
-        'east': east_bound,
-        'south': south_bound,
-        'west': west_bound,
-        'outputFormat': 'GTiff',
-        'API_Key': OPEN_TOPOGRAPHY_API_KEY
-    }
-)
+def reproject_dem(src_filename, dst_filename, dst_crs):
+    with rasterio.open(src_filename) as src:
+        transform, width, height = calculate_default_transform(
+            src.crs,
+            dst_crs,
+            src.width,
+            src.height,
+            *src.bounds
+        )
+        dst_meta = src.meta.copy()
+        dst_meta.update({
+            'crs': dst_crs,
+            'transform': transform,
+            'width': width,
+            'height': height,
+        })
+        with rasterio.open(dst_filename, 'w', **dst_meta) as dst:
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.cubic
+                )
 
-print(response)
+
+def convert_dem_to_image(dem_filename, image_filename):
+    with rasterio.open(dem_filename) as src:
+        data = src.read(1)
+        min_elevation = np.min(data)
+        max_elevation = np.max(data)
+        normalized = (data - min_elevation) / (max_elevation - min_elevation)
+        discretized = (normalized * np.iinfo(np.uint16).max).astype(np.uint16)
+        image = Image.fromarray(discretized)
+        image.save(image_filename)
+
+
+with open('../config.json') as file:
+    mountains = json.load(file)
+
+for mountain in mountains:
+    dem_filename = os.path.join('data/dems', mountain['id'] + '.tif')
+    mercator_filename = os.path.join('data/mercator_dems', mountain['id'] + '.tif')
+    heightmap_filename = os.path.join('data/heightmaps', mountain['id'] + '.png')
+
+    get_dem(dem_filename, mountain['coords'], 1000)
+    reproject_dem(dem_filename, mercator_filename, CRS.from_epsg(3857))
+    convert_dem_to_image(mercator_filename, heightmap_filename)
