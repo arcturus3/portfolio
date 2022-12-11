6db83a8d949d0229dd5b45cd3e5e584c87d9fea4
Arti Schmidt <artischmidt3@gmail.com>
2022-12-11 22:56:01 +0100

    Fix scene update

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,9 +4,12 @@ import {Scene} from './scene';
 export class Renderer {
   renderer;
   scene;
+  clock;
 
   constructor(scene: Scene) {
+    this.render = this.render.bind(this);
     this.scene = scene;
+    this.clock = new THREE.Clock();
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
     });
@@ -17,8 +20,9 @@ export class Renderer {
     return this.renderer.domElement;
   }
 
-  start() {
-    requestAnimationFrame(this.start);
+  render() {
+    requestAnimationFrame(this.render);
+    this.scene.update(this.clock.getDelta());
     this.renderer.render(this.scene, this.scene.getCamera());
   }
 }
