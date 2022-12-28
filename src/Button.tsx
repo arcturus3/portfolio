18b17fe328eb66307f2d719935df085fb95447a3
Arti Schmidt <artischmidt3@gmail.com>
2022-12-28 22:56:50 +0100

    Handle button text overflow

--- src/Button.tsx
+++ src/Button.tsx
@@ -14,13 +14,19 @@ export const Button = (props: ButtonProps) => {
       className={classNames(
         'flex',
         'gap-3',
+        'items-center',
         props.iconPosition === 'left' ? 'flex-row' : 'flex-row-reverse',
       )}
       onClick={props.onClick}
     >
       <>
         <props.icon />
-        <span className='font-mono text-lg opacity-75'>{props.text}</span>
+        <span className='
+          font-mono text-lg opacity-75 mt-0.5 max-[420px]:max-w-[100px]
+          whitespace-nowrap overflow-clip text-ellipsis
+        '>
+          {props.text}
+        </span>
       </>
     </button>
   );
