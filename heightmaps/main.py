0a0fbaf8fe591a2946d99b1c342de697906fd07e
Arti Schmidt <artischmidt3@gmail.com>
2023-01-01 05:59:59 +0100

    Remove unused code in heightmap generation

--- heightmaps/main.py
+++ heightmaps/main.py
@@ -9,7 +9,6 @@ import rasterio
 from rasterio.crs import CRS
 from rasterio.warp import calculate_default_transform, reproject, Resampling
 from PIL import Image
-import easing_functions
 
 
 def get_dem(dem_filename, center, radius):
@@ -95,43 +94,6 @@ def rescale_elevations(heightmap, size_meters):
     return elevation_range * pixels_per_meter * normalize_heightmap(heightmap)
 
 
-def dropoff(radius, factor, x):
-    dist = factor * radius
-    inner_radius = radius - dist
-    ease = easing_functions.CubicEaseInOut(start=1, end=0, duration=dist)
-    clamped_x = np.clip(x, inner_radius, radius)
-    return ease(clamped_x - inner_radius)
-
-
-def _apply_dropoff(heightmap, dropoff_factor):
-    size_pixels = heightmap.shape[0]
-    center = (size_pixels - 1) / 2
-    result_heightmap = np.zeros_like(heightmap)
-    for y in range(size_pixels):
-        for x in range(size_pixels):
-            dist = np.linalg.norm([x - center, y - center])
-            result_heightmap[y][x] = heightmap[y][x] * dropoff(center, dropoff_factor, dist)
-    return result_heightmap
-
-
-def apply_dropoff(heightmap, dropoff_factor):
-    size_pixels = heightmap.shape[0]
-    center = (size_pixels - 1) / 2
-    result_heightmap = np.zeros_like(heightmap)
-    for y in range(size_pixels):
-        for x in range(size_pixels):
-            dist = np.linalg.norm([x - center, y - center])
-            if dist <= 50:
-                factor = 1
-            elif dist <= 100:
-                factor = 2 - dist / 50
-            else:
-                factor = 0
-            result_heightmap[y][x] = heightmap[y][x] * factor
-    return result_heightmap
-
-
-
 def encode_heightmap(heightmap):
     data = base64.b64encode(heightmap)
     return bytes.decode(data)
@@ -151,14 +113,10 @@ for mountain in mountains:
     reproject_dem(dem_filename, mercator_filename, CRS.from_epsg(3857))
 
     heightmap = read_dem(mercator_filename)
-    write_image(os.path.join('data/heightmaps1', mountain['id'] + '.png'), heightmap)
     heightmap = resize_heightmap(heightmap, size_pixels)
-    write_image(os.path.join('data/heightmaps2', mountain['id'] + '.png'), heightmap)
     heightmap = rescale_elevations(heightmap, mountain['radius'] * 2)
-    write_image(os.path.join('data/heightmaps3', mountain['id'] + '.png'), heightmap)
-    # heightmap = apply_dropoff(heightmap, dropoff_factor)
-    write_image(os.path.join('data/heightmaps4', mountain['id'] + '.png'), heightmap)
     mountain['heightmap'] = encode_heightmap(heightmap)
+    write_image(os.path.join('data/heightmaps', mountain['id'] + '.png'), heightmap)
 
 with open('../generated/mountains.json', 'w') as file:
     json.dump(mountains, file)
