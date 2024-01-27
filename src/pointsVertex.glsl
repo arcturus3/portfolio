e5beaa240c60dde35993d9f70451acf22e17dbe9
Arti <artischmidt3@gmail.com>
2024-01-27 13:50:40 -0500

    Fix point display on Windows

--- src/pointsVertex.glsl
+++ src/pointsVertex.glsl
@@ -4,7 +4,7 @@ varying float frag_opacity;
 
 void main() {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    // gl_PointSize = 1.5;
+    gl_PointSize = 1.;
     frag_position = vec3(position);
     frag_opacity = opacity;
 }
